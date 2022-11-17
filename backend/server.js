9564ba44d089b6eba55247ff67a50996934c40e0
fwd370 <fwd1@berkeley.edu>
2022-11-17 19:42:18 +0000

    DB connected

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 //app
@@ -14,7 +15,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 //cors
-app.use(cors());
+if(process.env.NODE_ENV == 'development') {
+	app.use(cors({origin: `${process.env.CLIENT_URL}`}));
+}
 
 //routes
 app.get('/api', (req, res) => {
@@ -26,3 +29,9 @@ const port = process.env.PORT || 8000
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`)
 });
+
+//db
+mongoose
+	.connect(process.env.DATABASE_LOCAL) // Mongoose 6.0 onwards assumes useNewUrlParser, useCreateIndex are true, useFindAndModify is false.
+	.then(() => console.log("DB connected"))
+	.catch((err) => console.log("DB Error => ", err));
