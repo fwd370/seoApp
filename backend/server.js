a27dd6319ad9e193a6c0e45b8085297fd286ba16
fwd370 <fwd1@berkeley.edu>
2022-11-27 02:04:36 +0800

    add signup

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,40 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//bring in routes
+const blogRoutes = require("./routes/blog");
+const authRoutes = require("./routes/auth");
 
 //app
 const app = express();
 
 // middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 //cors
-if(process.env.NODE_ENV == 'development') {
-	app.use(cors({origin: `${process.env.CLIENT_URL}`}));
+if (process.env.NODE_ENV == "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 
-//routes
-app.get('/api', (req, res) => {
-	res.json({time: Date().toString()})
-});
+//routes middleware
+app.use("/api", blogRoutes);
+app.use("/api", authRoutes);
 
 //port
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`);
 });
 
 //db
 mongoose
-	.connect(process.env.DATABASE_LOCAL) // Mongoose 6.0 onwards assumes useNewUrlParser, useCreateIndex are true, useFindAndModify is false.
-	.then(() => console.log("DB connected"))
-	.catch((err) => console.log("DB Error => ", err));
+  .connect(process.env.DATABASE_LOCAL) // Mongoose 6.0 onwards assumes useNewUrlParser, useCreateIndex are true, useFindAndModify is false.
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
